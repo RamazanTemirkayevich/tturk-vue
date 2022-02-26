74cddd001ed8964a3380536342c7b2c95d4d6eaa
Family <family@MacBook-Air-Family.local>
2022-02-26 11:40:08 +0300

    first vue com

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <HelloWorld/>
+  <Product/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Product,
+  }
+}
+</script>
+
+<style>
+
+@font-face {
+    font-family: 'GT Eesti Pro Display';
+    src: url('fonts/GT\ Eesti\ Pro\ Display-Thin.woff2');
+    font-style: normal;
+    font-weight: 100;
+}
+
+@font-face {
+    font-family: 'GT Eesti Pro Display';
+    src: url('fonts/GT\ Eesti\ Pro\ Display-Light.woff2');
+    font-style: normal;
+    font-weight: 200;
+}
+
+@font-face {
+    font-family: 'GT Eesti Pro Display';
+    src: url('fonts/GT\ Eesti\ Pro\ Display-Regular.woff2');
+    font-style: normal;
+    font-weight: 300;
+}
+
+@font-face {
+    font-family: 'GT Eesti Pro Display';
+    src: url('fonts/GT\ Eesti\ Pro\ Display-Medium.woff2');
+    font-style: normal;
+    font-weight: 500;
+}
+
+@font-face {
+    font-family: 'GT Eesti Pro Display';
+    src: url('fonts/GT\ Eesti\ Pro\ Display-Bold.woff2');
+    font-style: normal;
+    font-weight: 700;
+}
+
+* {
+    font-family: 'GT Eesti Pro Display';
+    margin: 0;
+    padding: 0;
+    font-weight: 300;
+}
+
+h3 {
+    font-weight: 300;
+}
+
+ul {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+
+.container {
+    padding-left: 115px;
+    padding-right: 115px;
+    max-width: calc(128rem + 40px);
+}
+</style>
